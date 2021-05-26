dcff8c71c646bbe534d83a143900aec291318557
Abel Limas <limas9@icloud.com>
2021-05-26 15:43:50 -0500

    starting session 4

---
+++ session-4/assignments/hw/inventory_app/productapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+
+    AUTO        = 'Auto'
+    BEAUTY      = 'Beauty'
+    GROCERIES   = 'Groceries'
+    OUTDOORS    = 'Outdoors'
+    TECH        = 'Tech'
+
+    CATEGORY_CHOICES = [
+        (AUTO, 'Auto'),
+        (BEAUTY, 'Beauty'),
+        (GROCERIES, 'Groceries'),
+        (OUTDOORS, 'Outdoors'),
+        (TECH, 'Tech'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price =  models.FloatField()
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=TECH)
+    photo = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
