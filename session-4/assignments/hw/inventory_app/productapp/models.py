1649020479c31ff57d407708c327c3d5c8c6c6c0
Abel Limas <limas9@icloud.com>
2021-05-27 01:50:53 -0500

    having issues turning category to it's own class. TODO: add user field to products; add categories and foreign key; list categories and based on category; note: mysql migration was completed in session-2

--- session-4/assignments/hw/inventory_app/productapp/models.py
+++ session-4/assignments/hw/inventory_app/productapp/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=200, db_index=True)
+#     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+#
+#     class Meta:
+#         # ordering = ('name',)
+#         verbose_name = 'category'
+#         verbose_name_plural = 'categories'
+#
+#     def __str__(self):
+#         return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('productapp:product_list_by_category', args=[self.slug])
+
+
 # Create your models here.
 class Product(models.Model):
 
@@ -21,6 +38,8 @@ class Product(models.Model):
     description = models.CharField(max_length=200)
     price =  models.FloatField()
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=TECH, blank=True)
+    # category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
     photo = models.ImageField(upload_to='products', blank=True) # TODO: fix photo not being able to load in form
 
     def __str__(self):
--- session-4/assignments/hw/inventory_app/productapp/urls.py
+++ session-4/assignments/hw/inventory_app/productapp/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('catalog/<pk>/product-update', ProductUpdateView.as_view(), name='product-update'),
     path('catalog/<pk>/product-delete', ProductDeleteView.as_view(), name='product-delete'),
 
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="registration/logout.html"), name='logout'),
 ]
